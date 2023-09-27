4628f2e25f7c9be4d0b2bdf654d48e6607e94ed7
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-27 13:19:39 +0300

    debugged the project

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, Response
 import pickle
-
+from sklearn.preprocessing import StandardScaler
 # Create a Flask instance
 app = Flask(__name__)
 
 # load the pickle model
-model = pickle.load(open('model.pkl', 'rb'))
+with open('model.pkl', 'rb') as file:
+    model, scaler = pickle.load(file)
 
 @app.route("/")
 def home():
@@ -14,11 +15,26 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
-    prediction = model.predict(features)
+    try:
+        sepal_length = float(request.form.get('Sepal_Length'))
+        sepal_width = float(request.form.get('Sepal_Width'))
+        petal_length = float(request.form.get('Petal_Length'))
+        petal_width = float(request.form.get('Petal_Width'))
+        
+        # Perform your prediction here using the input values
+        arr = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+        scaled_data = scaler.transform(arr)
+        predictions = model.predict(scaled_data)
+        print(f"Prediction Result: {predictions}")
+        response =  jsonify({"Prediction": predictions.tolist()})
+        response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '0'
+        return render_template('index.html', prediction_text=predictions)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-    return render_template('index.html', prediction_text  = 'The flower species is {}'.format(prediction))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pickle
+from joblib import dump
 
 #load the dataset
 
@@ -16,11 +17,12 @@ print(df.head())
 X = df[['Sepal_Length', 'Sepal_Width', 'Petal_Length', 'Petal_Width']]
 y = df['Class']
 # Split the data into train and validation set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=123)
 
 # feature scaling
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
+
 X_test = sc.transform(X_test)
 
 # Instantiate the model
@@ -30,4 +32,5 @@ classifier = RandomForestClassifier()
 classifier.fit(X_train, y_train)
 
 # Make a pickle file for our model
-pickle.dump(classifier, open('model.pkl', 'wb'))
+with open('model.pkl', 'wb') as file:
+    pickle.dump((classifier, sc), file)
